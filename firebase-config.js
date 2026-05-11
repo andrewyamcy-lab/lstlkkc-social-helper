aef0b311b8d5fd7c04a2e6bedf2372fa3af159cf
andrewyamcy-lab <andrewyamcy@gmail.com>
2026-05-11 12:26:12 +0800

    Add Firebase config for Google login and cloud save

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCbr9IqNFqU4i6H9gRt_dhiVhMUQipx7y4",
+  authDomain: "lstlkkc-social-helper.firebaseapp.com",
+  projectId: "lstlkkc-social-helper",
+  storageBucket: "lstlkkc-social-helper.firebasestorage.app",
+  messagingSenderId: "424379032704",
+  appId: "1:424379032704:web:c0bb9fb1421853bce8b265",
+  measurementId: "G-20SQMVEX08"
+};
